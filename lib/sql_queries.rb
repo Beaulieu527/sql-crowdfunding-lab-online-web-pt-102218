cc28ee6440bcefda222b35d6028650173a9f8583
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-22 01:33:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,18 +16,17 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT title, SUM(pledges.amount)
     FROM projects
-    INNER JOIN pledges
+    JOIN pledges
     ON projects.id = pledges.project_id
     GROUP BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT
-    FROM
-    JOIN
-    ON
-    GROUP BY 
-  "
+  "SELECT users.name, users.age, SUM(pledges.amount) 
+    FROM users
+    JOIN pledges
+    ON pledges.user_id = users_id
+    GROUP BY name "
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
