c4e64309b4db866182710ee6edf6655b00cff3cb
Justin Beaulieu <beaulieudev@gmail.com>
2019-01-22 00:53:29 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,4 +3,3 @@ CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, fundin
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
 CREATE TABLE users (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
-
